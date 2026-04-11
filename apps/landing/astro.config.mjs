a967eccecff0309ce5f22353adc8a86b7877537a
usman-faisal <usman.shamsi987@gmail.com>
2026-04-11 12:24:15 +0500

    landing page updates

--- apps/landing/astro.config.mjs
+++ apps/landing/astro.config.mjs
@@ -2,7 +2,9 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), icon()]
 });
